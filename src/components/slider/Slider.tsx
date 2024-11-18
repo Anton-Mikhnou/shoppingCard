62dc3efad021d579473811598f070537e14cb58a
Anton Mikhnou <anton_mikhnov03@mail.ru>
2024-11-18 17:02:20 +0300

    finished slider for dekstop

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,7 +1,7 @@
 import style from './Slider.module.scss'
 import ButtonNext from '../../UI/buttonNext/ButtonNext'
 import ButtonPrev from '../../UI/buttonPrev/ButtonPrev'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 interface SliderArray {
     id: number,
@@ -10,7 +10,7 @@ interface SliderArray {
 }
 
 export default function Slider() {
-    const [index, setIndex] = useState<number>(0)
+    const [index, setIndex] = useState<number>(0);
 
     const sliderArray: Array<SliderArray> = [
         {
@@ -19,26 +19,23 @@ export default function Slider() {
             title: 'The new phones are here take a look.'
         },
         {
-            id: 0,
+            id: 1,
             img: 'img/iPhone-14-Plus.jpg',
             title: 'Think different, everything different.'
         },
         {
-            id: 0,
+            id: 2,
             img: 'img/970743.webp',
             title: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
         }
     ]
-
-
-    let advertisement = sliderArray[index];
 
     let hasPrev = index > 0;
     let hasNext = index < sliderArray.length - 1;
 
     function HandleClickNext(): void {
         if(hasNext) {
-            setIndex(index + 1);
+            setIndex((index) => (index + 1) % sliderArray.length);
         } else {
             setIndex(0);
         }
@@ -46,7 +43,7 @@ export default function Slider() {
     
     function HandleClickPrev(): void {
         if(hasPrev) {
-            setIndex(index - 1);
+            setIndex((index) => (index - 1) % sliderArray.length);
         } else {
             setIndex(2);
         }
@@ -54,11 +51,19 @@ export default function Slider() {
 
     function backgroundColor(): string {
         if(index === 0) {
-            return '#dce5e2'
+            return '#dce5e2';
         } else if (index === 1) {
-            return '#e1e9fc'
+            return '#e1e9fc';
         } else {
-            return '#eee6fd'
+            return '#eee6fd';
+        }
+    }
+
+    function buttonColor(key:number): string {
+        if(key === index) {
+            return 'white';
+        } else {
+            return 'black';
         }
     }
 
@@ -67,15 +72,26 @@ export default function Slider() {
             <div className={style['container-Slider']}>
                 <div className={style['container__inner']}>
                     <ButtonNext func={HandleClickNext}/>
-                    <div className={style['description']} style={{backgroundColor:backgroundColor()}}>
-                        <h1>
-                            {advertisement.title}
-                        </h1>
-                    </div>
-                    <div className={style['picture']}>
-                        <img src={advertisement.img}/>
-                    </div>
+                    {sliderArray.map((slider) => {
+                        return (
+                            <div key={slider.id} className={style['slider-content']} style={{ transform: `translateX(-${index * 100}%)`}}>
+                                <div className={style['description']} style={{backgroundColor: backgroundColor()}}>
+                                    <h1>
+                                        {slider.title}
+                                    </h1>
+                                </div>
+                                <div className={style['picture']}>
+                                    <img src={slider.img}/>
+                                </div>
+                            </div>
+                        )
+                    })}
                     <ButtonPrev func={HandleClickPrev}/>
+                    <div className={style['button-container']}>
+                        <button key={0} onClick={() => setIndex(0)} style={{backgroundColor: buttonColor(0)}}></button>
+                        <button key={1} onClick={() => setIndex(1)} style={{backgroundColor: buttonColor(1)}} ></button>
+                        <button key={2} onClick={() => setIndex(2)} style={{backgroundColor: buttonColor(2)}}></button>
+                    </div>
                 </div>
             </div>
         </>
